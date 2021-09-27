121a32769f90feb673fd5e71900694d4ea96f55f
Sahibuddin Ahmed <sahibuddin@gmail.com>
2021-09-27 12:07:06 +0530

    Initial Commit

---
+++ PortfolioGenerator/PortfolioGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioGeneratorApp.swift
+//  PortfolioGenerator
+//
+//  Created by Sahibuddin Ahmed on 27/09/21.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
